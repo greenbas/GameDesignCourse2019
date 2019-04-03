5c3cb07694d99ef38c9f6692222dede5d44c0214
Steven Greenbank <greenbas@udml10.cs.uregina.ca>
2019-04-03 11:14:55 -0600

    final lab

---
+++ New Unity Project/Assets/LabFinal/Manager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+
+    [SerializeField]
+    public Transform UIPanel;
+    bool isPaused; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIPanel.gameObject.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+        {
+            Pause();
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && isPaused)
+        {
+            UnPause();
+        }
+    }
+    public void Pause()
+    {
+        isPaused = true;
+        UIPanel.gameObject.SetActive(true);
+
+    }
+    public void UnPause()
+    {
+        isPaused = false;
+        UIPanel.gameObject.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
